cffc72fa121d41ff8b0ea244f2e77a02d4c5d43d
Torabis <sadihome@gmail.com>
2021-03-07 22:57:01 -0800

    add style .css

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+const api = {
+  key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  return (
+    <div className="app">
+      <main>
+<div className= "search-box">
+  <input type="text" className= "search-bar" placeholder= "Search"/>
+</div>
+      </main>
+    </div>
+
+  );
+}
+
+export default App;
